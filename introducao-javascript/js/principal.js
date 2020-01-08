986a25946f7338c019b8fb591971068191e45bae
mbj-junior <mbj_junior@hotmail.com>
2020-01-07 23:23:47 -0300

    estudo 07012020

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -23,13 +23,14 @@ for (var i = 0; i < pacientes.length; i++) {
 		console.log("peso invalido");
 		pesoEhValido = false;
 		tdImc.textContent = "Peso Invalido";
-		paciente.style.color = "red";
+		paciente.classList.add("paciente-invalido")
 	}
 
 	if (altura < 0 || altura > 3.0) {
 		console.log("autura invalida");
 		alturaEhValida = false;
 		tdImc.textContent = "Altura Invalida"
+		paciente.classList.add("paciente-invalido")
 	}
 
 
@@ -41,8 +42,50 @@ for (var i = 0; i < pacientes.length; i++) {
 
 }
 
+var botãoAdicionar = document.querySelector("#adicionar-paciente");
+
+botãoAdicionar.addEventListener("click", function(event){
+	event.preventDefault();
+
+	var form = document.querySelector("#form-adiciona");
+
+	var nome = form.nome.value;
+	var peso = form.peso.value;
+	var altura = form.altura.value;
+	var gordura = form.gordura.value;
+
+	var pacienteTr = document.createElement("tr");
+		var nomeTd = document.createElement("td");
+		var pesoTd = document.createElement("td");
+		var alturaTd = document.createElement("td");
+		var gorduraTd = document.createElement("td");
+		var imcTd = document.createElement("td");
+
+	nomeTd.textContent = nome;
+	pesoTd.textContent = peso;
+	alturaTd.textContent = altura;
+	gorduraTd.textContent = gordura;
+
+	pacienteTr.appendChild(nomeTd);
+	pacienteTr.appendChild(pesoTd);
+	pacienteTr.appendChild(alturaTd);
+	pacienteTr.appendChild(gorduraTd);
+
+	var tabela = document.querySelector("#tabela-pacientes");
+	tabela.appendChild(pacienteTr);
 
 
+});
+
+
+
+titulo.addEventListener("click", function(){
+	console.log("Função anonima");
+});
+
+function mostraMensagem(){
+	console.log("olá eu fui clicado");
+}
 
 
 
