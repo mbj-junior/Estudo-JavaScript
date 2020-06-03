1779d41bf362670f81ceb822e2c64282a3e6c303
Marcio Botelho Jr <mbj_junior@hotmail.com>
2020-06-03 20:48:48 -0300

    Estudo sobre ArrowFunctions

---
+++ DevPleno/ArrowFunctions.js
@@ -0,0 +1,24 @@
+//funções anonimas
+//this está referenciando internamente
+const func1 = function(param1){
+
+}
+
+//tem esse nome pela seta "=>"
+// ela tem instruções dentro dela
+//this está referenciando externamente
+const func11 = (param)=>{
+
+}//expression body
+const func2 = function(valor){
+    return valor *2
+}
+const func2 = (valor) => valor *2
+const func2 = valor => valor *2 //quando se tem somente um valor podemos reduzir assim
+
+const vetor = [1,2,3]
+vetor
+    .map(valor=>valor*2)
+    .map // podemos acrescentar mais execuções de modo mais simples
+
+
