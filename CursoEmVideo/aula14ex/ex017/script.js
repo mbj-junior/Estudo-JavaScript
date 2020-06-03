def56cd608b4d874521da35eeab5b0f7e84d7909
Marcio Botelho Jr <mbj_junior@hotmail.com>
2020-06-03 15:55:01 -0300

    Exercício 17 aula 14

--- CursoEmVideo/aula14ex/ex017/script.js
+++ CursoEmVideo/aula14ex/ex017/script.js
@@ -1,1 +1,20 @@
-alert('ola')
+function tabuada(){
+    let numElem = document.getElementById('txtNumero')
+    let tabuElem = document.getElementById('selecTabuada')
+
+    if (numElem.value.length == 0){
+        window.alert('Digite um numero')
+    }else{
+        let numDigitado = Number(numElem.value) 
+        tabuElem.innerHTML = '' //limpar a tabuada antes de iniciar
+
+        for (let i = 1; i <= 10; i++) {
+            let item = document.createElement('option')
+            item.text = `${numDigitado} x ${i} = ${numDigitado*i}`
+            item.value = `tabuada${i}`
+            tabuElem.appendChild(item)
+        }
+
+    }
+
+}
