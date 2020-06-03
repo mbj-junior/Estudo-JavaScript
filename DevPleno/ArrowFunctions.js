685a46ec0a4be7e03a1c02a822b9c45be58d6473
Marcio Botelho Jr <mbj_junior@hotmail.com>
2020-06-03 20:52:59 -0300

    aprendendo

--- DevPleno/ArrowFunctions.js
+++ DevPleno/ArrowFunctions.js
@@ -5,7 +5,7 @@ const func1 = function(param1){
 }
 
 //tem esse nome pela seta "=>"
-// ela tem instruções dentro dela mesma
+// ela tem instruções dentro dela
 //this está referenciando externamente
 const func11 = (param)=>{
 
